ac4c6c43e6ed6a8eede0fbfc38743f1fec4fe9dc
SoUrAv <aansourav@gmail.com>
2024-04-03 15:03:58 +0600

    Converting to NextJS done

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,7 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="dark:bg-body bg-white font-[Sora] dark:text-white text-dark">
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,28 @@
+import MovieCard from "@/components/MovieCard";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <main>
+      <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <Sidebar />
+        <div className="content">
+          <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
