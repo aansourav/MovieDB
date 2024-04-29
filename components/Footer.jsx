0fa71d60b8a9bb1855263b157cf19e786a6f0244
SoUrAv <aansourav@gmail.com>
2024-04-29 15:48:34 +0600

    Footer updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,12 @@
-
 const Footer = () => {
     return (
         <footer className="py-6 md:py-8 mt-8">
             <div className="container mx-auto">
                 <p className="text-center text-sm text-black/30 dark:text-[#EEEEEE]/60">
-                    Copyright ©2024 | All rights reserved by Learn with Sumit
+                    Copyright ©2024 | All rights reserved by{" "}
+                    <a href="https://github.com/aansourav" target="_blank">
+                        Abdullah An-Noor
+                    </a>
                 </p>
             </div>
         </footer>
